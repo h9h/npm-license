eaf27c9c35dfd8b457b2800202d34d2c78698e43
heinke <h9h@users.noreply.github.com>
2017-09-13 19:01:27 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const fs = require(‚fs‘);
+const path = require(‚path‘)
+
+const traverseDirectory = (dirname, callback) => {
+  let directory = [];
+
+  fs.readdir(dirname, function(err, list) {
+    dirname = fs.realpathSync(dirname);
+    if (err) {
+      return callback(err)
+    }
+
+    let listlength = list.length;
+    list.forEach(function(file) {
+      file = path.resolve(dirname, file)
+
+      fs.stat(file, function(err, stat) {
+        directory.push(file);
+
+        if (stat && stat.isDirectory()) {
+          traverseDirectory(file, function(err, parsed) {
+            directory = directory.concat(parsed);
+            if (!—listlength) {
+              callback(null, directory);
+            }
+          })
+        } else {
+          if (!—listlength) {
+            callback(null, directory);
+          }
+        }
+      })
+    })
+  })
+}
+
+traverseDirectory(__dirname, function(err, result) {
+  if (err) {
+    console.log(err);
+    return
+  }
+
+  console.log(result);
+});
